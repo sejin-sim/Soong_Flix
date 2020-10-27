11aa5aae209f8a5097dc22792eb22423e5caebec
심세진 <ssj259@gmail.com>
2020-10-27 13:43:42 +0000

    ~10/27

--- application.py
+++ application.py
@@ -25,13 +25,9 @@ def get_value():
         data.append(request.form.get('Q5'))
         data.append(request.form.get('Q6'))
         data.append(request.form.get('Q7'))
-        # if len(data)<7: 선택한 갯수가 7보다 적으면 에러 팝업창을 띄워준다.
-        #     return
-        print(data)
+        print(data)                         # test 용
         MV = mv_API.get_data(data)
-
-        return '결과 화면 output' # return render_template('result.html', poster = MV)
-                                 # {{poster}} in result.html 
+        return render_template('soongflix_result.html', poster = MV)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=int(sys.argv[1]))
--- mv_API.py
+++ mv_API.py
@@ -1,47 +1,50 @@
 import random
 
-def get_data(data): # 사용자가 입력한 정보를 list 형태로 반환하는 함수
-  new_data = []
+def get_data(data): # 사용자가 입력한 정보를 영화 포스터로 반환하는 함수
+      new_data = []
 
-  value_set = [
-      {"1" : "로맨스",
-        "2" : "코미디",
-        "3" : "액션",
-        "4" : "공포",
-        "5" : "SF"},
+      value_set = [
+          {"1" : "로맨스",
+            "2" : "코미디",
+            "3" : "액션",
+            "4" : "공포",
+            "5" : "SF"},
 
-       {"1" : random.randrange(2016,2021),
-        "2" : random.randrange(2011,2016),
-        "3" : random.randrange(2006,2011),
-        "4" : random.randrange(2000,2006)},
+           {"1" : random.randrange(2016,2021),
+            "2" : random.randrange(2011,2016),
+            "3" : random.randrange(2006,2011),
+            "4" : random.randrange(2000,2006)},
 
-      {"1" : "돈",
-        "2" : "로봇",
-        "3" : "동물",
-        "4" : "총"},
-      
-      {"1" : "도시",
-        "2" : "시골",
-        "3" : "산",
-        "4" : "절"},
-      
-       {"1" : "잠",
-        "2" : "노래",
-        "3" : "음식",
-        "4" : "바다"},
-      
-       {"1" : "10대",
-        "2" : "20대",
-        "3" : "30대",
-        "4" : "40대"}, 
-      
-       {"1" : "한국",
-        "2" : "미국",
-        "3" : "일본",
-        "4" : "프랑스"}]
+          {"1" : "돈",
+            "2" : "로봇",
+            "3" : "동물",
+            "4" : "총"},
 
-  for i in range(len(data)):
-     new_data.append(value_set[i][data[i]])
-  print(new_data)
+          {"1" : "도시",
+            "2" : "시골",
+            "3" : "산",
+            "4" : "절"},
+
+           {"1" : "잠",
+            "2" : "노래",
+            "3" : "음식",
+            "4" : "바다"},
+
+           {"1" : "10대",
+            "2" : "20대",
+            "3" : "30대",
+            "4" : "40대"}, 
+
+           {"1" : "한국",
+            "2" : "미국",
+            "3" : "일본",
+            "4" : "프랑스"}]
+
+      for i in range(len(data)):
+         new_data.append(value_set[i][data[i]])
+      print(new_data                                   # test 용
+    
+      poster_url = 'https://sites.google.com/site/gamewithprocessing2/_/rsrc/1456492182946/home/%ED%91%9C%EC%A7%80.png?height=400&width=268'
+      return poster_url
 
 # audiAcc	누적관람인원 : 50만원
